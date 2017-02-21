634fc4d5ae073f8977024121a5f5703613d33264
thomas <thneang@gmail.com>
2017-02-21 03:14:02 +0100

    Création d'une fenêtre Qt

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <Shape.hpp>
-#include <Point.hpp>
-#include <Line.hpp>
 #include <Ellipse.hpp>
 #include <Polygon.hpp>
-#include <Circle.hpp>
+#include <QApplication>
+#include <QPushButton>
 
+// TODO Faire des tests unitaires, rapporte des points dans le rapport
 
-int main() {
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+
+    QPushButton drawing("Hello world!");
+
+    drawing.setFixedSize(1500, 900);
+    drawing.show();
+
     Point p(3,3);
     float rx = 2;
     float ry = 2;
     Ellipse e(p,rx,ry);
     std::cout << e.dist_origin() << std::endl;
-    return 0;
+    return app.exec();
 }
