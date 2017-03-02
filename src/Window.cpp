6ab4572ad984c33a6c3644688d4eeca7f2bf0356
kev <kevinblondel@msn.com>
2017-03-02 17:34:54 +0100

    probleme création ellipse, le point d'origine se place mal.

--- src/Window.cpp
+++ src/Window.cpp
@@ -9,6 +9,12 @@ Window::Window(){
     scene = new QGraphicsScene(this);
     setFixedSize(1500,900);
     this->setScene(scene);
+
+    //création d'une ligne diagonale transparente pour éviter le recadrage de la fenetre
+    QPen pen(Qt::transparent);
+    QLineF line(0,0,1500,900);
+    scene->addLine(line,pen);
+
 //    m_button_quit = new QPushButton("Quittez",this);
 //    m_button_quit->setToolTip("quittez l'application");
 //    m_button_quit->setFixedSize(100, 100);
@@ -25,6 +31,8 @@ Window::~Window(){
 void Window::mouseReleaseEvent(QMouseEvent *event){
     if(event->button() == Qt::LeftButton){
         queue_point.push_back(Point(event->pos().x(),event->pos().y()));
+        //affichage du point
+        //scene->addLine(event->pos().x(),event->pos().y(),event->pos().x(),event->pos().y());
         std::cout << event->pos().x() << " " << event->pos().y() << std::endl;
         switch(type_figure){
             case 0:
@@ -34,31 +42,52 @@ void Window::mouseReleaseEvent(QMouseEvent *event){
                     Point A = queue_point[0];
                     Point B = queue_point[1];
                     list_figure.push_back(new Line(A,B));
-                    QLineF qf(A.get_x(),A.get_y(),B.get_x(),B.get_y());
-                    std::cout << qf.x1() << " " << qf.y1() << " " << qf.x2() << " " << qf.y2() << std::endl;
-                    scene->addLine(qf);
-                    std::cout << scene->items().size() << std::endl;
+
+
+                    QPointF Af = mapToScene(static_cast<int>(A.get_x()),static_cast<int>(A.get_y()));
+                    QPointF Bf = mapToScene(static_cast<int>(B.get_x()),static_cast<int>(B.get_y()));
+
+                    scene->addLine(QLineF(Af,Bf));
+
+                    type_figure = 1;
                     queue_point.clear();
                 }
                 break;
             case 1:
                 //cercle
                 if(queue_point.size() == 2){
+                    std::cout << "create circle " << std::endl;
                     Point O = queue_point[0];
                     Point T = queue_point[1];
-                    list_figure.push_back(new Circle(O,distance(O,T)));
-                    scene->addEllipse(O.get_x(),O.get_y(),distance(O,T),distance(O,T));
+                    float OT = distance(O,T);
+                    list_figure.push_back(new Circle(O,OT));
+
+                    QPointF Of = mapToScene(static_cast<int>(O.get_x()),static_cast<int>(O.get_y()));
+                    QPointF Tf = mapToScene(static_cast<int>(T.get_x()),static_cast<int>(T.get_y()));
+                    float OTf = distance(Point((float)Of.x(),(float)Of.y()),Point((float)Tf.x(),(float)Tf.y()));
+                    scene->addEllipse(Of.x(),Of.y(),OTf,OTf);
+
                     queue_point.clear();
                 }
                 break;
             case 2:
                 //ellipse
                 if(queue_point.size() == 3){
+                    std::cout << "create ellipse " << std::endl;
                     Point O = queue_point[0];
                     Point A = queue_point[1];
                     Point B = queue_point[2];
                     list_figure.push_back(new Ellipse(O,distance(O,A),distance(O,B)));
-                    scene->addEllipse(O.get_x(),O.get_y(),distance(O,A),distance(O,B));
+                    std::cout << O.get_x() << "," << O.get_y() << std::endl;
+
+                    QPointF Of = mapToScene(static_cast<int>(O.get_x()),static_cast<int>(O.get_y()));
+                    QPointF Af = mapToScene(static_cast<int>(A.get_x()),static_cast<int>(A.get_y()));
+                    QPointF Bf = mapToScene(static_cast<int>(B.get_x()),static_cast<int>(B.get_y()));
+                    int OA = abs(static_cast<int>(Of.x() - Af.x()));
+                    int OB = abs(static_cast<int>(Of.y() - Bf.y()));
+                    std::cout << Of.x() << "," << Of.y() << " " << OA << " " << OB << std::endl;
+                    scene->addEllipse(Of.x(),Of.y(),OA,OB);
+
                     queue_point.clear();
                 }
 
